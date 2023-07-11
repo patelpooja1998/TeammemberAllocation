9901166d1fbc36163970bef4e478a98e2ba75dce
patelpooja1998 <31209808+patelpooja1998@users.noreply.github.com>
2023-07-11 02:09:38 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './header';
+import Employees from './Employees';
+import Footer from './footer';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
