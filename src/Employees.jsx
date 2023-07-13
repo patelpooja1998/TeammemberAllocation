1c5a13e07bb053d68579e53baf1f45fdfbacb306
patelpooja1998 <31209808+patelpooja1998@users.noreply.github.com>
2023-07-13 02:42:18 +0000

    Create Teams DropDown List

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -3,6 +3,8 @@ import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
+  const [selectedTeam, setTeam] = useState('TeamB');
+  
   const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
@@ -88,8 +90,27 @@ const Employees = () => {
     teamName: "TeamD"
   }
   ])
+
+  function handleTeamSelectionChange(event){
+    console.log();
+    setTeam(event.target.value)
+  }
+  
   return (
     <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-6">
+          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+          <option value="TeamA">Team A</option>
+          <option value="TeamB">Team B</option>
+          <option value="TeamC">Team C</option>
+          <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
+
+
+      
       <div class="row justify-content-center mt-3 mb-3">
         <div class="col-8">
           <div class="card-collection">
